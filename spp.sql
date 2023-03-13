384dd5765cecf3bd904ef4469e6a904f64323a95
CenzSmith18 <70206065+CenzSmith18@users.noreply.github.com>
2023-03-13 20:53:23 +0700

    Update spp.sql

--- spp.sql
+++ spp.sql
@@ -1,273 +1,177 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 13, 2023 at 08:10 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `spp`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.27-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
 -- --------------------------------------------------------
 
---
--- Table structure for table `failed_jobs`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+
+-- Dumping database structure for spp
+CREATE DATABASE IF NOT EXISTS `spp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `spp`;
+
+-- Dumping structure for table spp.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(255) NOT NULL,
   `connection` text NOT NULL,
   `queue` text NOT NULL,
   `payload` longtext NOT NULL,
   `exception` longtext NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table spp.failed_jobs: ~0 rows (approximately)
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
 
---
--- Table structure for table `kelas`
---
-
-CREATE TABLE `kelas` (
-  `id_kelas` int(11) NOT NULL,
+-- Dumping structure for table spp.kelas
+CREATE TABLE IF NOT EXISTS `kelas` (
+  `id_kelas` int(11) NOT NULL AUTO_INCREMENT,
   `nama_kelas` varchar(10) NOT NULL,
-  `kompetensi_keahlian` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `kompetensi_keahlian` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_kelas`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `kelas`
---
-
+-- Dumping data for table spp.kelas: ~2 rows (approximately)
+/*!40000 ALTER TABLE `kelas` DISABLE KEYS */;
 INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kompetensi_keahlian`) VALUES
-(4, 'XII RPL', 'Rekayasa Perangkat Lunak'),
-(5, 'XII RPL 2', 'Rekayasa Perangkat Lunak');
+	(4, 'XII RPL', 'Rekayasa Perangkat Lunak'),
+	(5, 'XII RPL 2', 'Rekayasa Perangkat Lunak');
+/*!40000 ALTER TABLE `kelas` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
+-- Dumping structure for table spp.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `migration` varchar(255) NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `migrations`
---
-
+-- Dumping data for table spp.migrations: ~4 rows (approximately)
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `password_reset_tokens`
---
-
-CREATE TABLE `password_reset_tokens` (
+-- Dumping structure for table spp.password_reset_tokens
+CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
   `email` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table spp.password_reset_tokens: ~0 rows (approximately)
+/*!40000 ALTER TABLE `password_reset_tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_reset_tokens` ENABLE KEYS */;
 
---
--- Table structure for table `personal_access_tokens`
---
+-- Dumping structure for table spp.pembayaran
+CREATE TABLE IF NOT EXISTS `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT,
+  `id_petugas` int(11) NOT NULL,
+  `nisn` varchar(10) NOT NULL,
+  `tgl_bayar` date DEFAULT NULL,
+  `bulan_dibayar` varchar(8) NOT NULL,
+  `tahun_dibayar` varchar(4) NOT NULL,
+  `id_spp` int(11) NOT NULL,
+  `jumlah_bayar` int(25) NOT NULL,
+  PRIMARY KEY (`id_pembayaran`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `personal_access_tokens` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+-- Dumping data for table spp.pembayaran: ~0 rows (approximately)
+/*!40000 ALTER TABLE `pembayaran` DISABLE KEYS */;
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_petugas`, `nisn`, `tgl_bayar`, `bulan_dibayar`, `tahun_dibayar`, `id_spp`, `jumlah_bayar`) VALUES
+	(2, 1, '2312', '2023-03-13', '03', '2023', 4, 250000);
+/*!40000 ALTER TABLE `pembayaran` ENABLE KEYS */;
+
+-- Dumping structure for table spp.personal_access_tokens
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `tokenable_type` varchar(255) NOT NULL,
-  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
   `token` varchar(64) NOT NULL,
   `abilities` text DEFAULT NULL,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table spp.personal_access_tokens: ~0 rows (approximately)
+/*!40000 ALTER TABLE `personal_access_tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personal_access_tokens` ENABLE KEYS */;
 
---
--- Table structure for table `siswa`
---
-
-CREATE TABLE `siswa` (
+-- Dumping structure for table spp.siswa
+CREATE TABLE IF NOT EXISTS `siswa` (
   `nisn` char(10) NOT NULL,
   `nis` char(8) DEFAULT NULL,
   `nama` varchar(35) NOT NULL,
   `id_kelas` int(11) NOT NULL,
   `alamat` text NOT NULL,
   `no_telp` varchar(13) NOT NULL,
-  `id_spp` int(11) DEFAULT NULL
+  `id_spp` int(11) DEFAULT NULL,
+  PRIMARY KEY (`nisn`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table spp.siswa: ~1 rows (approximately)
+/*!40000 ALTER TABLE `siswa` DISABLE KEYS */;
+INSERT INTO `siswa` (`nisn`, `nis`, `nama`, `id_kelas`, `alamat`, `no_telp`, `id_spp`) VALUES
+	('2312', '23221', 'Wanda Nugraha', 4, 'Jl.Peta', '08972363', 4);
+/*!40000 ALTER TABLE `siswa` ENABLE KEYS */;
 
---
--- Table structure for table `spp`
---
-
-CREATE TABLE `spp` (
+-- Dumping structure for table spp.spp
+CREATE TABLE IF NOT EXISTS `spp` (
   `id_spp` int(11) NOT NULL,
   `tahun` int(11) NOT NULL,
-  `nominal` int(11) NOT NULL
+  `nominal` int(11) NOT NULL,
+  KEY `id_spp` (`id_spp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `spp`
---
-
+-- Dumping data for table spp.spp: ~1 rows (approximately)
+/*!40000 ALTER TABLE `spp` DISABLE KEYS */;
 INSERT INTO `spp` (`id_spp`, `tahun`, `nominal`) VALUES
-(4, 2021, 100000);
+	(4, 2019, 150000);
+/*!40000 ALTER TABLE `spp` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
+-- Dumping structure for table spp.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
   `email` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) NOT NULL,
+  `nama_petugas` varchar(35) DEFAULT NULL,
   `level` enum('admin','petugas') DEFAULT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `users`
---
+-- Dumping data for table spp.users: ~2 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `email`, `email_verified_at`, `password`, `nama_petugas`, `level`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'admin', 'admin@spp.com', NULL, '$2y$10$eFPaf3vSo.dRciR8lPG8u.4r9IFVz6gJOclgVblC7M79xmS7N6hY2', 'Admin', 'admin', NULL, '2023-03-13 01:25:10', '2023-03-13 01:25:10'),
+	(2, 'petugas', 'petugas@spp.com', NULL, '$2y$10$c4E5fqwh5gwAqbjYO4C9u.a.wmgO/sBAKssH8YwdAS4xCCAA.DhcC', 'Petugas', 'petugas', NULL, '2023-03-13 01:25:10', '2023-03-13 01:25:10');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `level`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin User', 'admin@spp.com', NULL, '$2y$10$eFPaf3vSo.dRciR8lPG8u.4r9IFVz6gJOclgVblC7M79xmS7N6hY2', 'admin', NULL, '2023-03-12 18:25:10', '2023-03-12 18:25:10'),
-(2, 'Manager User', 'manager@spp.com', NULL, '$2y$10$c4E5fqwh5gwAqbjYO4C9u.a.wmgO/sBAKssH8YwdAS4xCCAA.DhcC', 'petugas', NULL, '2023-03-12 18:25:10', '2023-03-12 18:25:10');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indexes for table `kelas`
---
-ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id_kelas`);
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `password_reset_tokens`
---
-ALTER TABLE `password_reset_tokens`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indexes for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
-
---
--- Indexes for table `siswa`
---
-ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`nisn`);
-
---
--- Indexes for table `spp`
---
-ALTER TABLE `spp`
-  ADD PRIMARY KEY (`id_spp`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `kelas`
---
-ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `spp`
---
-ALTER TABLE `spp`
-  MODIFY `id_spp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
